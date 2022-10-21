e23593f41244b52577148b2740e8f637c9f57ceb
kodeman2 <holuwakoladey@gmail.com>
2022-10-21 08:01:45 +0100

    updated the main section

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Topbar from "./components/topbar/Topbar"
+import Sidebar from "./components/sidebar/Sidebar"
+import "./app.css"
+import Home from "./pages/home/Home";
+
+function App() {
+  
+return (
+<div>
+<Topbar/>
+<div className="container">
+ <Sidebar />
+ <Home />
+</div>
+</div>
+);
+
+
+}
+
+export default App;
---
+++ src/components/charts/Chart.jsx
@@ -0,0 +1,9 @@
+import "./chart.css";
+
+export default function Chart() {
+  return (
+    <div className='chart'>
+Chart
+    </div>
+  )
+}
