41a40c803e5acaf03b70f673a588855e2a053816
kodeman2 <holuwakoladey@gmail.com>
2022-11-07 23:54:05 +0100

    chore:user page develpment

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,27 @@ import Topbar from "./components/topbar/Topbar"
 import Sidebar from "./components/sidebar/Sidebar"
 import "./app.css"
 import Home from "./pages/home/Home";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import UserList from "./pages/userList/UserList";
 
 function App() {
   
 return (
-<div>
+    <Router>
 <Topbar/>
 <div className="container">
  <Sidebar />
- <Home />
+ <Routes>
+   <Route path="/" element={<Home />} />
+   <Route path="/users" element={<UserList />} />
+   
+
+
+ </Routes>
+ 
+ 
 </div>
-</div>
+    </Router>
 );
 
 
---
+++ src/pages/UserList/UserList.jsx
@@ -0,0 +1,12 @@
+import './UserList.css';
+import { DataGrid } from '@mui/x-data-grid';
+
+export default function UserList() {
+  return (
+    <div className='userList'>
+     
+
+    </div>
+  )
+}
+
