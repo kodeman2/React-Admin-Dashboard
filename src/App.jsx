ff2c06354f00462a477d7788aa9fd7d5caff3b8e
kodeman2 <holuwakoladey@gmail.com>
2022-11-08 22:12:56 +0100

    chore:updates added

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,23 @@
-import Topbar from "./components/topbar/Topbar"
-import Sidebar from "./components/sidebar/Sidebar"
-import "./app.css"
+import Topbar from "./components/topbar/Topbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import "./app.css";
 import Home from "./pages/home/Home";
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 
 function App() {
-  
-return (
+  return (
     <Router>
-<Topbar/>
-<div className="container">
- <Sidebar />
- <Routes>
-   <Route path="/" element={<Home />} />
-   <Route path="/users" element={<UserList />} />
-   
-
-
- </Routes>
- 
- 
-</div>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/users" element={<UserList />} />
+        </Routes>
+      </div>
     </Router>
-);
-
-
+  );
 }
 
 export default App;
--- src/pages/UserList/UserList.jsx
+++ src/pages/UserList/UserList.jsx
@@ -1,12 +1,63 @@
-import './UserList.css';
-import { DataGrid } from '@mui/x-data-grid';
+import "./userList.css";
+import { DataGrid } from "@mui/x-data-grid";
+
 
 export default function UserList() {
+
+ const columns = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "firstName",
+    headerName: "First name",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "lastName",
+    headerName: "Last name",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "age",
+    headerName: "Age",
+    type: "number",
+    width: 110,
+    editable: true,
+  },
+  {
+    field: "fullName",
+    headerName: "Full name",
+    description: "This column has a value getter and is not sortable.",
+    sortable: false,
+    width: 160,
+    
+  },
+];
+
+const rows = [
+  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+];
+
   return (
-    <div className='userList'>
-     
-
+    <div className="userList">
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+        disableSelectionOnClick
+        experimentalFeatures={{ newEditingApi: true }}
+      />
     </div>
-  )
+  );
 }
-
