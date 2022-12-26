2528f86b2a5d883afd2fe56673427fa79ba7f14c
kodeman2 <holuwakoladey@gmail.com>
2022-12-26 11:34:11 +0100

    added new feature

--- src/components/featuredinfo/Featuredinfo.jsx
+++ src/components/featuredinfo/Featuredinfo.jsx
@@ -1,42 +1,36 @@
-import './featuredinfo.css';
-import { ArrowDownward, ArrowUpward } from '@mui/icons-material';
+import "./featuredinfo.css";
+import { ArrowDownward, ArrowUpward } from "@mui/icons-material";
 
-export default function Featuredinfo() {
+export default function FeaturedInfo() {
   return (
-    <div className='featured'>
-<div className="featuredItem">
-  <span className="featuredTitle">Revenue</span>
-  <div className="featuredMoneyContainer">
-    <span className="featuredMoney">$2,895</span>
-    <span className="featuredMoneyRate">
-     -11.4 <ArrowDownward  className='featuredIcon negative'/>
-    </span>
-  </div>
-  <span className="featuredSub">Compared to last Month</span>
-</div>
-<div className="featuredItem">
-  <span className="featuredTitle">Sales</span>
-  <div className="featuredMoneyContainer">
-    <span className="featuredMoney">$8,839</span>
-    <span className="featuredMoneyRate">
-     -1.4 <ArrowDownward className='featuredIcon negative' />
-    </span>
-  </div>
-  <span className="featuredSub">Compared with last Month</span>
-</div>
-<div className="featuredItem">
-  <span className="featuredTitle">Cost</span>
-  <div className="featuredMoneyContainer">
-    <span className="featuredMoney">$2,239</span>
-    <span className="featuredMoneyRate">
-     2.4
- 
-      <ArrowUpward  className='featuredIcon' />
-      
-    </span>
-  </div>
-  <span className="featuredSub">Compared to last Month</span>
-</div>
+    <div className="featured">
+     <div className="featuredItem">
+      <span className="featuredTitle">Revenue</span>
+      <div className="featuredMoneyContainer">
+       <span className="featuredMoney">$3,213</span>
+       <span className="featuredMoneyRate">-13.6 <ArrowDownward className="featuredIcon negative" /></span>
+      </div>
+      <span className="featuredSub">Compared to last month</span>
+     </div>
+     <div className="featuredItem">
+      <span className="featuredTitle">Sales</span>
+      <div className="featuredMoneyContainer">
+       <span className="featuredMoney">$4,740</span>
+       <span className="featuredMoneyRate">-2.3 <ArrowDownward className="featuredIcon negative" /></span>
+     </div>
+     <span className="featuredSub">Compared to last month</span> 
+     </div>
+     <div className="featuredItem">
+      <span className="featuredTitle">Cost</span>
+      <div className="featuredMoneyContainer">
+       <span className="featuredMoney">$1,322</span>
+       <span className="featuredMoneyRate">+3.6 <ArrowUpward className="featuredIcon" /></span>
+      </div>
+      <span className="featuredSub">Compared to last month</span>
+
+     </div>
     </div>
-  );
+  )
 }
+
+
